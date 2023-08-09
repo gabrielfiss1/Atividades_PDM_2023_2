767b593b7348d42c574f86101a0ddd1ee0b74d6c
Gabriel Fiss <gabriel--fiss@hotmail.com>
2023-08-09 16:57:54 -0300

    useState contador

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { Colors } from 'react-native/Libraries/NewAppScreen';
 function MyButton(props){
   return (
     <TouchableHighlight style={styles.button} onPress={() => props.onClick()}>
-      <Text>{props.texto}</Text>
+      <Text style={styles.textoButton}>{props.texto}</Text>
     </TouchableHighlight>
   )
 }
@@ -21,23 +21,29 @@ export default function App() {
 
   const decrementar = () => {
     setContador(contador - 1)
+  }
+  const resetar = () => {
+    setContador(0)
 
   }
 
   return (
     <View style={styles.container}>
-      <Text>Valor = {contador}</Text>
+      <Text style={styles.texto}>{contador}</Text>
       <StatusBar style="auto" />
       <MyButton 
        texto="Incrementar"
        onClick={contar}  
+       backgroundColor={'red'}
       />
-      <MyButton />
       <MyButton 
        texto="Decrementar"
        onClick={decrementar}
       />
-      <MyButton />
+      <MyButton 
+       texto="Resetar"
+       onClick={resetar}
+      />
     </View>
   );
 }
@@ -45,17 +51,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#EADBC8',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
 
   button: {
-    backgroundColor: '#f194ff',
+    backgroundColor: '#900C3F',
+    width: '75%',
+    height:100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5,
+    margin: 25,
+    borderRadius: 10,
+    fontSize: 50,
+    borderWidth: 2,
+    borderColor: '#C70039',
    
   },
 
+  textoButton: {
+    fontSize: 40,
+    color: '#F8F0E5'
+  },
+
   texto: {
-    fontSize: 20,
+    fontSize: 100,
+    color: 'red',
+    marginBottom: 75,
   }
 })
